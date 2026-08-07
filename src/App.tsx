5168ce535ea35897512123f8c118eda7267a5a0a
KohJiaLer <jkoh0036@student.monash.edu>
2026-08-07 22:54:47 +0800

    chore: initialise React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <h1>
+        Cheapest Basket
+      </h1>
+      <p>
+        Compare shop prices and find out whether a second stop is worth it.
+      </p>
+    </main>
+  );
+}
+
+export default App;
